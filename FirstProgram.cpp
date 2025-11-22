a84bf7e19504450f5a36321c437a553047a7971a
ManishKumarSaini20 <manish.kumar.here@gmail.com>
2025-11-22 13:23:54 +0530

    cpp file is modified

--- FirstProgram.cpp
+++ FirstProgram.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
+using namespace std;
+
+void add(int a, int b)
+{
+    int c = a+b;
+    cout<<"c = "<<c<<endl;
+}
 
 int main()
 {
     std::cout<<"Hello Manish.\n";
+    add(10,20);
     return 0;
 }
