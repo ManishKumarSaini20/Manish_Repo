8de1292d0e54f65d9569fbefcec120f5d92b4950
ManishKumarSaini20 <manish.kumar.here@gmail.com>
2025-11-23 12:21:07 +0530

    Functor function addition

--- FirstProgram.cpp
+++ FirstProgram.cpp
@@ -1,4 +1,7 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
 using namespace std;
 
 void callbackadd(int a, int b)
@@ -14,10 +17,23 @@ void InitF(void (*callbackFunc)(int, int), int a, int b)
     callbackFunc(a,b);
 }
 
+class CManish
+{
+public:
+    bool operator()(int x)
+    {
+        return x<0?true:false;
+    }
+};
+
+using MYVECT = vector<int>;
 int main()
 {
     std::cout<<"Hello Manish.\n";
     callbackadd(10,20);
     InitF(callbackadd, 10, 20);
+    MYVECT Vect1 {1,2,3,-4,5,6,-7};
+    MYVECT::iterator it = find_if(Vect1.begin(), Vect1.end(), CManish());
+    cout<<"*it = "<<*it<<endl;
     return 0;
 }
