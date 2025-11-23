c552a743ace692d17cd1c1e639db79730700ae23
ManishKumarSaini20 <manish.kumar.here@gmail.com>
2025-11-23 11:22:59 +0530

    Added callback function

--- FirstProgram.cpp
+++ FirstProgram.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 using namespace std;
 
-void add(int a, int b)
+void callbackadd(int a, int b)
 {
     int c = a+b;
     cout<<"c = "<<c<<endl;
 }
 
+void InitF(void (*callbackFunc)(int, int), int a, int b)
+{
+    a = a + 5;
+    b = b + 5;
+    callbackFunc(a,b);
+}
+
 int main()
 {
     std::cout<<"Hello Manish.\n";
-    add(10,20);
+    callbackadd(10,20);
+    InitF(callbackadd, 10, 20);
     return 0;
 }
