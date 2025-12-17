5f2b7a2c26ea229662dc169d1347e8a93d4e7654
ManishKumarSaini20 <manish.kumar.here@gmail.com>
2025-12-17 19:13:06 +0530

    newb

--- lambda_func_main.cpp
+++ lambda_func_main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using MYVECT = std::vector<int>;
 
 void printvector(MYVECT vect)
@@ -46,5 +47,21 @@ int main()
     std::cout<<"vect2 is printing.\n";
     printvector(vect2);
     // now write the code for the STL algorithms and try to put the lambda functions in a class
+    MYVECT vect3 {4,7,1,9,5,2};
+    std::cout<<"Print vector without sort.\n";
+    printvector(vect3);
+    std::sort(vect3.begin(), vect3.end(), [](const int& x, const int& y){return x > y?true:false;});
+    std::cout<<"Print vector after sort.\n";
+    printvector(vect3);
+    MYVECT::iterator it =  std::find_if(vect3.begin(), vect3.end(),[](const int& x){return x == 9?true:false;});
+    if(it != vect3.end())
+    {
+        std::cout<<"*it = "<<*it<<std::endl;
+    }   
+    std::cout<<"Original vector.\n";
+    printvector(vect3);
+    std::for_each(vect3.begin(), vect3.end(), [](int& x ){x += 5;});
+    std::cout<<"Vector after adding 5 to each variable.\n";
+    printvector(vect3);
     return 0;
 }
