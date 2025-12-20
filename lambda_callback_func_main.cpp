27174af74050c134332b7c9da139d53b412c31c5
ManishKumarSaini20 <manish.kumar.here@gmail.com>
2025-12-20 14:48:48 +0530

    newb

--- lambda_callback_func_main.cpp
+++ lambda_callback_func_main.cpp
@@ -11,6 +11,16 @@ void printvector(MYVECT vect)
     }
 }
 
+bool ascendingvector(const int& a, const int& b)
+{
+    return (a<b)?true:false;
+}
+
+bool descendingvector(const int& a, const int& b)
+{
+    return (a>b)?true:false;
+}
+
 int main()
 {
     auto result_lam = [](int x)
@@ -63,5 +73,16 @@ int main()
     std::for_each(vect3.begin(), vect3.end(), [](int& x ){x += 5;});
     std::cout<<"Vector after adding 5 to each variable.\n";
     printvector(vect3);
+    //Vector sorting with ascending and descending with callback function
+    MYVECT vect4{3,6,1,9,11,2,16,4};
+    std::cout<<"vect4 before sorting.\n";
+    printvector(vect4);
+    std::sort(vect4.begin(), vect4.end(), ascendingvector);
+    std::cout<<"vect4 after ascending order.\n";
+    printvector(vect4);
+    std::sort(vect4.begin(), vect4.end(), descendingvector);
+    std::cout<<"vect4 after descending order.\n";
+    printvector(vect4);
+    // Call a callback function with void return and void parameter
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -146,29 +146,29 @@ int main()
     objtuple_pair_return_multi_val->tuple_get_make();
     int iSize = 0;
     objtuple_pair_return_multi_val->tuple_size(iSize);
-    std::cout<<"iSize = "<<iSize<<std::endl;
+    std::cout << "iSize = " << iSize << std::endl;
     objtuple_pair_return_multi_val->tuple_swap();
     objtuple_pair_return_multi_val->tuple_cat();
     objtuple_pair_return_multi_val->tuple_tie();
-    std::pair<int,char> myPair = objtuple_pair_return_multi_val->func_retrun_pair();
-    std::cout<<"myPair.first = "<<myPair.first<<" myPair.second = "<<myPair.second<<std::endl;
+    std::pair<int, char> myPair = objtuple_pair_return_multi_val->func_retrun_pair();
+    std::cout << "myPair.first = " << myPair.first << " myPair.second = " << myPair.second << std::endl;
     int aa = 0;
     char cc{};
     float ff = 0;
-    std::tie(aa,cc,ff) = objtuple_pair_return_multi_val->func_retrun_tuple();
-    std::cout<<"aa = "<<aa<<" cc = "<<cc<<" ff = "<<ff<<std::endl;
+    std::tie(aa, cc, ff) = objtuple_pair_return_multi_val->func_retrun_tuple();
+    std::cout << "aa = " << aa << " cc = " << cc << " ff = " << ff << std::endl;
     std::unique_ptr<MANISH::ROOPA::VIRAT::retfromfunc> objretfromfunc = std::make_unique<MANISH::ROOPA::VIRAT::retfromfunc>();
     objtuple_pair_return_multi_val->funcreturningstruct(objretfromfunc.get());
-    std::cout<<"objretfromfunc->a = "<<objretfromfunc->a<<" objretfromfunc->c = "<<objretfromfunc->c<<" objretfromfunc->f = "<<objretfromfunc->f<<std::endl;
-    std::array <int,5> objarray = objtuple_pair_return_multi_val->funcreturningarray();
-    for(int& value_arr : objarray)
+    std::cout << "objretfromfunc->a = " << objretfromfunc->a << " objretfromfunc->c = " << objretfromfunc->c << " objretfromfunc->f = " << objretfromfunc->f << std::endl;
+    std::array<int, 5> objarray = objtuple_pair_return_multi_val->funcreturningarray();
+    for (int &value_arr : objarray)
     {
-        std::cout<<value_arr<<std::endl;
+        std::cout << value_arr << std::endl;
     }
     std::vector<int> myvect = objtuple_pair_return_multi_val->funcreturningvector();
-    for(int& value_vect : myvect)
+    for (int &value_vect : myvect)
     {
-        std::cout<<value_vect<<std::endl;
+        std::cout << value_vect << std::endl;
     }
     int aaa{0};
     char ccc;
@@ -184,10 +184,10 @@ int main()
     /*
     callback_function functionality
     */
-   std::unique_ptr<MANISH::ROOPA::VIRAT::callback_function> objcallback_function = std::make_unique<MANISH::ROOPA::VIRAT::callback_function>();
-   const int aaaa = 10;
-   const int bbbb = 20;
-   int returnval = 0;
-   objcallback_function->printval(MANISH::ROOPA::VIRAT::callback_function::addfunc, aaaa,bbbb);
+    std::unique_ptr<MANISH::ROOPA::VIRAT::callback_function> objcallback_function = std::make_unique<MANISH::ROOPA::VIRAT::callback_function>();
+    const int aaaa = 10;
+    const int bbbb = 20;
+    int returnval = 0;
+    objcallback_function->printval(MANISH::ROOPA::VIRAT::callback_function::addfunc, aaaa, bbbb);
     return 0;
 }
